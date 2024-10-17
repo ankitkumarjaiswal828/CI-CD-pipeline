7a28434f780e2f864a9e39d8a600b24a664f3f01
ankit kumar <aj24197@gmaile.com>
2024-10-17 23:45:36 +0530

    final commit

---
+++ src/main/java/com/example/sp_project_01/SpProject01Application.java
@@ -0,0 +1,15 @@
+package com.example.sp_project_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpProject01Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(SpProject01Application.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/sp_project_01/SpProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sp_project_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
